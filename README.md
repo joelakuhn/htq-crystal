8eac8d5911ed7702c2d8c1affbfa00d479f80f60
Joel <joel@joels-laptop-2.indtech.edu>
2019-07-18 15:54:18 -0400

    First Commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# HTQ
+
+HTQ is a simple tool for extracting elements from html streams using CSS3 selectors. It uses the incomarable (lexborisov/myhtml)[https://github.com/lexborisov/myhtml] wrapper (kostya/myhtml)[https://github.com/kostya/myhtml].
+
+## Building
+
+```shell
+shards install
+shards build --release
+```
+
+## Usage
+
+```
+usage: htq [files] [options]
+    -c QUERY, --css=QUERY            Specify a css query
+    -p, --pretty                     Pretty print output
+    -h, --help                       Print help message
+```
+
+## Examples
+
+```
+$ echo "<div><p>Lorem Ipsum</p></div>" | bin/htq -c p
+<p>Lorem Ipsum</p>
+$ echo "<div><p>Lorem Ipsum</p></div>" | bin/htq -p -c p
+<p>
+  Lorem Ipsum
+</p>
+```
---
+++ src/htq.cr
@@ -0,0 +1,86 @@
+require "myhtml"
+require "option_parser"
+
+module HTQ
+
+  class Config
+
+    property pretty = false
+    property css_queries = [] of String
+
+  end
+
+  config = Config.new()
+  files = [] of String
+
+  OptionParser.parse(ARGV) do |parser|
+
+    parser.banner = "usage: htq [files] [options]"
+
+    parser.on("-c QUERY", "--css=QUERY", "Specify a css query") do |query|
+      config.css_queries.push(query)
+    end
+
+    parser.on("-p", "--pretty", "Pretty print output") do
+      config.pretty = true
+    end
+
+    parser.on("-h", "--help", "Print help message") do
+      puts parser.to_s
+      exit();
+    end
+
+    parser.unknown_args do |positional_args|
+      files = positional_args
+    end
+
+  end
+
+  def self.process_input(input, config)
+
+    dom = Myhtml::Parser.new(input)
+
+    if config.css_queries.empty? && config.pretty
+
+      puts dom.to_pretty_html
+
+    else
+
+      config.css_queries.each do |query|
+
+        dom.css(query).each do |el|
+          if config.pretty
+            puts el.to_pretty_html
+          else
+            print el.to_html
+          end
+        end
+
+      end
+
+    end
+
+  end
+
+  if files.empty?
+
+    process_input(STDIN.gets_to_end(), config)
+
+  else
+
+    files.each do |file|
+      begin
+        if file == "-"
+          input = STDIN.gets_to_end()
+        else
+          input = File.read(file)
+        end
+        process_input(input, config)
+      rescue exception
+        STDERR.puts exception
+      end
+    end
+
+  end
+
+end
